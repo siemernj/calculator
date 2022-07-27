13f65228c533a7ee7be91513915d9b54d75983e0
Nick Siemer <siemernj@gmail.com>
2022-07-27 09:50:49 -0400

    Added number functionality to display, and AC button

--- script.js
+++ script.js
@@ -7,41 +7,72 @@ const operandButtons = document.querySelectorAll('[data-operand]');
 const equalButton = document.querySelector('[data-equals]');
 
 
-function clear () {
+numberButtons.forEach((button) => {
+    button.addEventListener('click', updateDisplay)
+});
+
+operandButtons.forEach((button) => {
+    button.addEventListener('click', chooseOperation)
+});
+
+allClearButton.addEventListener('click', allClear);
+
+deleteButton.addEventListener('click', del);
+
+
+function allClear () {
     currentDisplay.textContent = '0'
     previousDisplay.textContent = '0'
 }
 
 function del () {
+    currentDisplay.splice(-1,1)
 
 }
 
-function chooseOperation () {
+function chooseOperation (e) {
+    const key = e.target
+    const keyValue = key.textContent
+    const displayValue = currentDisplay.textContent
 
+    console.log(key)
 }
 
-function addNumber () {
-
+function add (a,b) {
+    return a + b;
 }
 
-function updateDisplay () {
-
+function subtract (a,b) {
+    return a - b;
 }
 
-numberButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-       
-    })    
-});
+function multiply (a,b) {
+    return a * b;
+}
 
-operandButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-     
-    })    
-});
+function divide (a,b) {
+    return a / b;
+}
 
 
 
-console.log(clear())
+
+
+function updateDisplay(e) {
+const key = e.target
+const keyValue = key.textContent
+const displayValue = currentDisplay.textContent
+    if (displayValue === '0') {
+        currentDisplay.textContent = keyValue
+    } else {
+        currentDisplay.textContent = displayValue + keyValue
+    }
+}
+
+
+
+
+
+
 
 
