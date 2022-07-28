3b2f26a8970083bbd6ef7a2c1c17c81ba70e81d3
Nick Siemer <siemernj@gmail.com>
2022-07-28 14:18:19 -0400

    Added operator functions, and button functionality to JS

--- script.js
+++ script.js
@@ -10,27 +10,51 @@ buttons.forEach((button) => {
             currentValue = Number(currentValue + button.innerText);
             current.innerText = currentValue;          
         }
-        else if (button.id ==='operator') {
-            chooseOperation()
+        else if (button.id ==='operand') {
+            currentValue = 0;
+            operator = button.innerText
+            previousValue = operations(previousValue, currentValue, operator);
+            previous.innerText += current.innerText + `${button.innerText}`;
+            current.innerText = currentValue;
+            
         }
         else if (button.id ==='allClear') {
-            allClear()
+            currentValue = 0;
+            previousValue = 0;
+            previous.innerText = 0;
+            current.innerText = 0;
         }
-
+        else if (button.id ==='delete') {
+            currentValue = Number(currentValue.toString().slice(0,-1));
+            current.innerText = currentValue;
+        }
+        else if (button.id ==='decimal') {
+            currentValue.toString().includes('.') ? currentValue : currentValue += '.';
+            current.innerText = currentValue;   
+        }
+        else if (button.id ==='equals') {
+            finalValue = operations(previousValue, currentValue, operator);
+            current.innerText = finalValue;
+            previous.innerText = null;
+        }
     });
 })
 
- function allClear() {
-    currentValue = 0;
-    current.innerText = 0
+function operations (a, b, operator) {
+    switch(operator) {
+        case '+':
+            return a + b;
+            break;
+        case '-':
+            return a - b;
+            break;
+        case '*':
+            return a * b;
+            break;
+        case '/':
+            return a / b;
+            break;
+    }
 }
- 
-
-
-
-function chooseOperation(e) {
-
-}
-
 
 
